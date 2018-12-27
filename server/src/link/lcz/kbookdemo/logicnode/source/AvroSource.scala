8101fdfbed396d52a0ccea22ef1dd88e5af35511
Felix <i@lcz.link>
2018-12-27 19:32:30 +0800

    fix AvroSource schema

--- server/src/link/lcz/kbookdemo/logicnode/source/AvroSource.scala
+++ server/src/link/lcz/kbookdemo/logicnode/source/AvroSource.scala
@@ -1,6 +1,8 @@
 package link.lcz.kbookdemo.logicnode.source
 
 import link.lcz.kbookdemo.logicnode.{BaseNode, Source}
+import net.liftweb.json.JObject
+import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 
 class AvroSource(env: Source.Environment) extends Source[AvroSource.Config](env) {
@@ -9,12 +11,15 @@ class AvroSource(env: Source.Environment) extends Source[AvroSource.Config](env)
   import org.apache.kafka.streams.scala.ImplicitConversions._
   import org.apache.kafka.streams.scala.Serdes._
 
-  override def outbound(idx: Int) = BaseNode.SchemaBound(env.ctx.stream[String, GenericRecord](config.topicName), ???)
+  // FIXME: need avro schema name
+  private val schema = new Schema.Parser().parse(net.liftweb.json.compactRender(config.schema))
+
+  override def outbound(idx: Int) = BaseNode.SchemaBound(env.ctx.stream[String, GenericRecord](config.topicName), schema)
 
 }
 
 object AvroSource {
 
-  case class Config(topicName: String)
+  case class Config(topicName: String, schema: JObject)
 
 }
