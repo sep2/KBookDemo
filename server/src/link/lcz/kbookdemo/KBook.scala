01dfcd1f4dd87b591c9f05cd341e831f8be85789
Felix <i@lcz.link>
2019-01-04 10:34:57 +0800

    update readme

--- server/src/link/lcz/kbookdemo/KBook.scala
+++ server/src/link/lcz/kbookdemo/KBook.scala
@@ -3,10 +3,7 @@ package link.lcz.kbookdemo
 import java.util.Properties
 
 import com.typesafe.scalalogging.LazyLogging
-import io.confluent.kafka.schemaregistry.client.{
-  CachedSchemaRegistryClient,
-  SchemaRegistryClient
-}
+import io.confluent.kafka.schemaregistry.client.{CachedSchemaRegistryClient, SchemaRegistryClient}
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
 import link.lcz.kbookdemo.logicnode.BaseNode
 import org.apache.avro.Schema
@@ -22,7 +19,7 @@ class KBook(ctx: KBook.Context) extends LazyLogging {
     import org.apache.kafka.streams.StreamsConfig
     p.put(StreamsConfig.APPLICATION_ID_CONFIG, s"kbook-$uuid")
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
-          ctx.config.meta.bootstrapServers)
+      ctx.config.meta.bootstrapServers)
 
     //p.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, ctx.schemaRegistry)
     //p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
@@ -40,7 +37,7 @@ class KBook(ctx: KBook.Context) extends LazyLogging {
 
   override def equals(o: Any): Boolean = o match {
     case kb: KBook => this.uuid == kb.uuid
-    case _         => false
+    case _ => false
   }
 
   override def hashCode(): Int = uuid.hashCode
@@ -55,15 +52,10 @@ object KBook extends LazyLogging {
     val ctx = KBook.Context(config)
 
     val linearized = config.dag.dag
-      .ensuring(_.isAcyclic,
-                throw new RuntimeException("The graph is not acyclic"))
-      .ensuring(_.isDirected,
-                throw new RuntimeException("The graph is directed"))
-      .ensuring(_.isConnected,
-                throw new RuntimeException("The graph is connected"))
-      .topologicalSort
-      .right
-      .getOrElse(throw new RuntimeException("The graph has no node"))
+      .ensuring(_.isAcyclic, throw new RuntimeException("The graph is not acyclic"))
+      .ensuring(_.isDirected, throw new RuntimeException("The graph is directed"))
+      .ensuring(_.isConnected, throw new RuntimeException("The graph is connected"))
+      .topologicalSort.right.getOrElse(throw new RuntimeException("The graph has no node"))
 
     // Since side effect is performed in the constructor of each LogicNode
     // we do not have to capture the result of this operation (unlike Spark)
@@ -78,9 +70,7 @@ object KBook extends LazyLogging {
           .collect { case (Some(node), edge) => node -> edge }
           .sortBy(_._2.toPort)
           .foldLeft(Seq[BaseNode.SchemaBound]()) { (ibs, x) =>
-            ibs :+ x._1
-              .asInstanceOf[logicnode.Predecessor]
-              .outbound(x._2.fromPort)
+            ibs :+ x._1.asInstanceOf[logicnode.Predecessor].outbound(x._2.fromPort)
           })
 
       val nodeConfig = current.toOuter
@@ -88,12 +78,9 @@ object KBook extends LazyLogging {
       import logicnode.{Sink, Source, Transformer}
 
       val newNode: BaseNode = nodeConfig.meta.`type` match {
-        case Dag.NodeType.Source =>
-          Source(clazz, Source.Environment(ctx, nodeConfig))
-        case Dag.NodeType.Transformer =>
-          Transformer(clazz, Transformer.Environment(ctx, nodeConfig, inbounds))
-        case Dag.NodeType.Sink =>
-          Sink(clazz, Sink.Environment(ctx, nodeConfig, inbounds))
+        case Dag.NodeType.Source => Source(clazz, Source.Environment(ctx, nodeConfig))
+        case Dag.NodeType.Transformer => Transformer(clazz, Transformer.Environment(ctx, nodeConfig, inbounds))
+        case Dag.NodeType.Sink => Sink(clazz, Sink.Environment(ctx, nodeConfig, inbounds))
       }
 
       constructed :+ newNode
@@ -105,14 +92,27 @@ object KBook extends LazyLogging {
   case class Context(private[KBook] val config: KBookConfig) {
     private[KBook] lazy val topology: Topology = builder.build()
     private val builder = new org.apache.kafka.streams.scala.StreamsBuilder
+    private val schemaRegistry: SchemaRegistryClient =
+      new CachedSchemaRegistryClient(
+        config.meta.schemaRegistry,
+        AbstractKafkaAvroSerDeConfig.MAX_SCHEMAS_PER_SUBJECT_DEFAULT,
+      )
 
     def stream[K, V](topic: String)(
-        implicit consumed: Consumed[K, V]): KStream[K, V] =
+      implicit consumed: Consumed[K, V]): KStream[K, V] =
       builder.stream[K, V](topic)
 
     def stream[K, V](topics: Set[String])(
-        implicit consumed: Consumed[K, V]): KStream[K, V] =
+      implicit consumed: Consumed[K, V]): KStream[K, V] =
       builder.stream[K, V](topics)
+
+    /*
+    * https://docs.confluent.io/current/schema-registry/docs/using.html#deleting-the-most-recently-registered-schema-under-subject-kafka-value
+    * https://docs.confluent.io/current/streams/developer-guide/datatypes.html#avro
+    * https://www.programcreek.com/java-api-examples/?code=confluentinc/ksql/ksql-master/ksql-engine/src/main/java/io/confluent/ksql/util/AvroUtil.java
+    */
+    def getValueSchema(topicName: String): Schema =
+      new Schema.Parser().parse(schemaRegistry.getLatestSchemaMetadata(topicName + "-value").getSchema)
 
     // FIXME: too many import needed
     object Serdes {
@@ -127,26 +127,12 @@ object KBook extends LazyLogging {
 
         import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
         gas.configure(java.util.Collections.singletonMap(
-                        AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
-                        config.meta.schemaRegistry),
-                      false)
+          AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+          config.meta.schemaRegistry),
+          false)
         gas
       }
     }
-
-    private val schemaRegistry: SchemaRegistryClient =
-      new CachedSchemaRegistryClient(
-        config.meta.schemaRegistry,
-        AbstractKafkaAvroSerDeConfig.MAX_SCHEMAS_PER_SUBJECT_DEFAULT,
-      )
-
-    /*
-    * https://docs.confluent.io/current/schema-registry/docs/using.html#deleting-the-most-recently-registered-schema-under-subject-kafka-value
-    * https://docs.confluent.io/current/streams/developer-guide/datatypes.html#avro
-    * https://www.programcreek.com/java-api-examples/?code=confluentinc/ksql/ksql-master/ksql-engine/src/main/java/io/confluent/ksql/util/AvroUtil.java
-    */
-    def getValueSchema(topicName: String): Schema =
-      new Schema.Parser().parse(schemaRegistry.getLatestSchemaMetadata(topicName + "-value").getSchema)
   }
 
 }
